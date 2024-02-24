03a89619de5bc7bd255234bdc47be042bc46d9f0
Alan Luna <alanjosuelunacardona@gmail.com>
2024-02-24 11:27:24 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//Console.WriteLine("Enunciado 1.1");
+//for (int n = 1; n <= 5; n++)
+//{
+//    Console.WriteLine(n);
+//}
+//Console.WriteLine("Enunciado 1.2");
+//int s = 0;
+//for (int d = 1; d <= 10; d++)
+//{
+//    Console.WriteLine(d);
+//    s =s+d;  
+//}
+//Console.WriteLine(s);
+
+//Console.WriteLine("Enunciado 2.1");
+//int x = 0;
+//while (x >= 0)
+//{
+//    Console.WriteLine("Ingresa un numero");
+//    x = int.Parse(Console.ReadLine());
+//}
+
+//Console.WriteLine("Enunciado 3.1");
+//string pass = "jutiapa";
+//string passI;
+//    do
+//{
+//    Console.WriteLine("Ingrese la contraseña: ");
+//    passI = Console.ReadLine().ToLower();
+
+//}while(passI != pass);
+//Console.WriteLine("Contraseña Correcta");
+
+
+
